31a5e0e567f76a08765e7ad3079bf22c3fe2648b
Ireo <ireo.ostacchini@gmail.com>
2020-08-17 15:39:42 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express = require('express');
 // Create a new express app instance
 const app: express.Application = express();
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 res.send('Hello World!');
 });
-app.listen(3000, function () {
+app.listen(3000, () => {
+// tslint:disable-next-line: no-console
 console.log('App is listening on port 3000!');
-}); 
+});
