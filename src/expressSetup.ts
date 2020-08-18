ab942607d77edb0924722188b0734a0a90b4fc60
Ireo <ireo.ostacchini@gmail.com>
2020-08-18 05:33:37 +0100

    .

--- src/expressSetup.ts
+++ src/expressSetup.ts
@@ -2,9 +2,17 @@ import express from "express";
 import methodOverride from "method-override";
 import bodyParser from "body-parser";
 import Controllers from "./controllers";
-//import container from "../inversify.config";
 import IBusiness from "./business/IBusiness";
 import { TYPES } from "./constants/types";
+
+
+import { Container } from "inversify";
+import Business from "./business";
+import IDb from "./db/IDb";
+import Db from "./db";
+import DbConnectionManager from "./db/dbConnectionManager";
+
+
 
 const router = express.Router();
 
@@ -13,6 +21,11 @@ const expressSetup = (app: any) => {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use("/api", router);
+
+  const container = new Container();
+container.bind<IBusiness>(TYPES.IBusiness).to(Business);
+container.bind<IDb>(TYPES.IDb).to(Db);
+//container.bind<DbConnectionManager>(TYPES.DbConnectionManager).toSelf();
 
   // const business = container.get<IBusiness>(TYPES.IBusiness);
 
